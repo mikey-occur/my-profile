70cff9a84cad7ba8bbb39c7f848109fd4ddb7cca
ThanhLong-FE <occurmikey@gmail.com>
2024-12-13 01:52:34 +0700

    Create Modal

---
+++ js/modal.js
@@ -0,0 +1,19 @@
+var btnOpen = document.querySelector('.open-modal-btn')
+var modal = document.querySelector('.modal')
+var iconClose = document.querySelector('.modal_header i')
+var btnClose = document.querySelector('.modal_footer button')
+
+function toggleModal(e){
+   /* console.log(e.target);  Find bug */ 
+    modal.classList.toggle('hide')
+}
+
+btnOpen.addEventListener('click', toggleModal)
+btnClose.addEventListener('click', toggleModal)
+iconClose.addEventListener('click', toggleModal)
+modal.addEventListener('click', function(e){
+    if(e.target == e.currentTarget){ /* currentTarget là gần target nhất */
+        toggleModal()
+    }
+})
+
